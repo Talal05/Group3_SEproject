3c888ceeb186bc691b59a8030007a1ec28f3cabe
Talal05 <118681298+Talal05@users.noreply.github.com>
2023-04-21 00:49:09 +0500

    first commit

--- Jobtal-main/node-server/db/connection.js
+++ Jobtal-main/node-server/db/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require("dotenv").config()
 
 // const dbURL = 'mongodb+srv://user:wiQd44tSAZg9O24Q@cluster0.eknly6e.mongodb.net/JobPortal?retryWrites=true&w=majority';
 
@@ -6,7 +7,7 @@ const mongoose = require('mongoose');
 
 // const dbURL  = 'mongodb+srv://muhammadhamza:hamza123@cluster0.ejy7buo.mongodb.net/jobtal?retryWrites=true&w=majority';
 
-const dbURL  = 'mongodb://muhammadhamza:hamza123@ac-7eqpmyw-shard-00-00.ejy7buo.mongodb.net:27017,ac-7eqpmyw-shard-00-01.ejy7buo.mongodb.net:27017,ac-7eqpmyw-shard-00-02.ejy7buo.mongodb.net:27017/jobtal?ssl=true&replicaSet=atlas-5y798y-shard-0&authSource=admin&retryWrites=true&w=majority';
+// const dbURL  = 'mongodb://muhammadhamza:hamza123@ac-7eqpmyw-shard-00-00.ejy7buo.mongodb.net:27017,ac-7eqpmyw-shard-00-01.ejy7buo.mongodb.net:27017,ac-7eqpmyw-shard-00-02.ejy7buo.mongodb.net:27017/jobtal?ssl=true&replicaSet=atlas-5y798y-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 // const dbURL  = 'mongodb+srv://hamza:hamza123@cluster0.dsmhzsu.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -15,7 +16,7 @@ const connectionParams = {
     useUnifiedTopology: true,
 };
 
-mongoose.connect(dbURL,connectionParams)
+mongoose.connect(process.env.dbURL,connectionParams)
     .then(()=> {
         console.info("Connected to DB")
     })
